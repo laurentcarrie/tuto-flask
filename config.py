81892854a8a4db749929af91810a9c827636c25d
Laurent Carrié <laurent.carrie@gmail.com>
2020-11-10 21:32:17 +0100

    sqlalchemy

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+from pathlib import Path
+basedir = str(Path(__file__).parent)
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
