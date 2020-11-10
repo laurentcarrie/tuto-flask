eff1e66f83831fab0b3ba3570f7ee6e2e0778c74
Laurent Carrié <laurent.carrie@gmail.com>
2020-11-10 22:33:52 +0100

    pylint

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from app import routes, models
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -6,7 +7,4 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
-
-
-from app import routes,models
+migrate = Migrate(app, db)
--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 from pathlib import Path
 basedir = str(Path(__file__).parent)
 
-class Config(object):
+
+class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
