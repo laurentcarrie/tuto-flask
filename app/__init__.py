789c3747cf79d7bff88430e694a5a21fa32db8a0
Laurent Carrié <laurent.carrie@gmail.com>
2020-11-11 09:51:32 +0100

    circular import

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
+# pylint:disable=E0401
 from flask_migrate import Migrate
+# pylint:enable=E0401
+
+from config import Config
+from app import routes
 
 app = Flask(__name__)
 app.config.from_object(Config)
