a2edf975c0376ca46589138b2dfafdb6f9394b47
Laurent Carrié <laurent.carrie@gmail.com>
2020-11-11 22:48:21 +0100

    chapter-5 ok

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# pylint:disable=E0401
 from flask_migrate import Migrate
-# pylint:enable=E0401
+from flask_login import LoginManager
 
 from config import Config
-from app import routes
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes  # noqa: E402
