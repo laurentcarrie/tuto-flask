2b512776764a856a2ed87239d826954dca8ee07d
Laurent Carrié <laurent.carrie@gmail.com>
2020-11-20 12:27:21 +0100

    test

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request, current_app, g
 from flask_login import current_user, login_required
 from app import db
-from app.models import User
-from app.main.forms import PostForm, EmptyForm, EditProfileForm
+from app.models import User, Message
+from app.main.forms import PostForm, EmptyForm, EditProfileForm, MessageForm
 from app.models import Post
 from flask_babel import _, get_locale
 from datetime import datetime
@@ -188,3 +188,34 @@ def user_popup(username):
     user = User.query.filter_by(username=username).first_or_404()
     form = EmptyForm()
     return render_template('user_popup.html', user=user, form=form)
+
+
+@bp.route('/send_message/<recipient>', methods=['GET', 'POST'])
+@login_required
+def send_message(recipient):
+    user = User.query.filter_by(username=recipient).first_or_404()
+    form = MessageForm()
+    if form.validate_on_submit():
+        msg = Message(author=current_user, recipient=user, body=form.message.data)
+        db.session.add(msg)
+        db.session.commit()
+        flash(_('Your message has been sent.'))
+        return redirect(url_for('main.user', username=recipient))
+    return render_template('send_message.html', title=_('Send Message'),
+                           form=form, recipient=recipient)
+
+
+@bp.route('/messages')
+@login_required
+def messages():
+    current_user.last_message_read_time = datetime.utcnow()
+    db.session.commit()
+    page = request.args.get('page', 1, type=int)
+    messages = current_user.messages_received.order_by(Message.timestamp.desc()).paginate(
+        page, current_app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for(
+        'main.messages', page=messages.next_num) if messages.has_next else None
+    prev_url = url_for(
+        'main.messages', page=messages.prev_num) if messages.has_prev else None
+    return render_template('message.html', messages=messages.items, next_url=next_url, prev_url=prev_url)
--- app/models.py
+++ app/models.py
@@ -31,6 +31,11 @@ class User(UserMixin, db.Model):
         backref=db.backref("followers", lazy="dynamic"),
         lazy="dynamic",
     )
+    messages_sent = db.relationship('Message', foreign_keys='Message.sender_id',
+                                    backref='author', lazy='dynamic')
+    messages_received = db.relationship('Message', foreign_keys='Message.recipient_id',
+                                        backref='recipient', lazy='dynamic')
+    last_message_read_time = db.Column(db.DateTime)
 
     def __repr__(self):
         return "<User {}>".format(self.username)
@@ -71,6 +76,12 @@ class User(UserMixin, db.Model):
             current_app.config["SECRET_KEY"],
             algorithm="HS256",
         ).decode("utf-8")
+
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        return Message.query.filter_by(recipient=self).filter(
+            Message.timestamp > last_read_time
+        ).count()
 
     @staticmethod
     def verify_reset_password_token(token):
@@ -144,3 +155,14 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return "<Post {}>".format(self.body)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Message {self.body}>'
