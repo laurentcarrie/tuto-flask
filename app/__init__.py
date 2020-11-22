0a1b861dbc6d64ccb29479259b006ce171876dd3
Laurent Carrié <laurent.carrie@gmail.com>
2020-11-22 11:28:35 +0100

    REST

--- app/__init__.py
+++ app/__init__.py
@@ -44,16 +44,16 @@ def create_app(config_class=Config):
     )
 
     from app.errors import bp as errors_bp
-
     app.register_blueprint(errors_bp)
 
     from app.auth import bp as auth_bp
-
     app.register_blueprint(auth_bp, url_prefix="/auth")
 
     from app.main import bp as main_bp
-
     app.register_blueprint(main_bp)
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
 
     app.redis = Redis.from_url(app.config['REDIS_URL'])
     app.task_queue = rq.Queue('microblog-tasks', connection=app.redis)
